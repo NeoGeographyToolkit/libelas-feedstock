c575efa1894fe159309223b9cbf9daa84fd8e766
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-06-19 17:08:11 -0700

    Add more deps to avoid conflicts

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,17 @@ export LDFLAGS="-L$PREFIX/lib"
 # Fix for missing liblzma
 perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
 
+if [ "$(uname)" = "Darwin" ]; then
+    EXT='.dylib'
+else
+    EXT='.so'
+fi
+
 mkdir -p build
 cd build
-cmake ..                                                   \
+cmake .. -DTIFF_LIBRARY_RELEASE="${PREFIX}/lib/libtiff${EXT}"
+
+make -j${CPU_COUNT}
 
 BIN_DIR=${PREFIX}/plugins/stereo/elas/bin
 mkdir -p ${BIN_DIR}
